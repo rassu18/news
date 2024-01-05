5589a3b5c411dfa1fcf61e6f90b0a9c1970638e9
rassu18 <105088829+rassu18@users.noreply.github.com>
2024-01-06 04:05:07 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import News from './components/News';
+
+function App() {
+  return (
+    <div className="App">
+      <News />
+    </div>
+  );
+}
+
+export default App;
