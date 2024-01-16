be93967236e7a90c300fe6604c35aa681e23aa57
rassu18 <105088829+rassu18@users.noreply.github.com>
2024-01-16 19:31:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ import News from "./components/News";
 import AboutUs from "./footer/AboutUs";
 import ContactUs from "./footer/ContactUsForm";
 import Disclaimer from "./footer/disclaimer";
-//import TermsConditions from "./TermsConditions";
+import banner from './images/banner.jpg'; 
+import Privacy from "./footer/privacy";// Import your banner image
+import TermsConditions from "./footer/terms";
 
 const App = () => {
   return (
+    <div>
+      <img className="container-fluid" src={banner} width="1200px" alt="Banner" />
     <Router>
       <Routes>
         <Route path="/" element={<News />} />
         <Route path="/about" element={<AboutUs />} />
          <Route path="/contact" element={<ContactUs />} />
          <Route path="/disclaimer" element={<Disclaimer />} />
-       {/* <Route path="/terms" element={<TermsConditions />} /> */}
+         <Route path="/privacy" element={<Privacy />} />
+         <Route path="/tnc" element={<TermsConditions />} /> 
       </Routes>
     </Router>
+    </div>
   );
 };
 
