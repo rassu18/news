ef94a3695df882df35cb7b0c26aa70ecb93a4315
rassu18 <105088829+rassu18@users.noreply.github.com>
2024-01-16 14:01:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import News from "./components/News";
 import AboutUs from "./footer/AboutUs";
 import ContactUs from "./footer/ContactUsForm";
+import Disclaimer from "./footer/disclaimer";
 //import TermsConditions from "./TermsConditions";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<News />} />
         <Route path="/about" element={<AboutUs />} />
          <Route path="/contact" element={<ContactUs />} />
+         <Route path="/disclaimer" element={<Disclaimer />} />
        {/* <Route path="/terms" element={<TermsConditions />} /> */}
       </Routes>
     </Router>
